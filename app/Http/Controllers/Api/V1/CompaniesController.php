6b27e7e81450cd94bc6f4b82d06f8f25e2815ffc
thiagocsgarcia <thiagocsgarcia@gmail.com>
2018-09-24 17:43:02 -0300

    primeiro commit

---
+++ app/Http/Controllers/Api/V1/CompaniesController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class CompaniesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Company::all();
+    }
+ 
+    public function show($id)
+    {
+        return Company::findOrFail($id);
+    }
+ 
+    public function update(Request $request, $id)
+    {
+        $company = Company::findOrFail($id);
+        $company->update($request->all());
+ 
+        return $company;
+    }
+ 
+    public function store(Request $request)
+    {
+        $company = Company::create($request->all());
+        return $company;
+    }
+ 
+    public function destroy($id)
+    {
+        $company = Company::findOrFail($id);
+        $company->delete();
+        return '';
+    }
+}
